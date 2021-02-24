d91e0acb846c7ee61337e9648daa415c367a3cab
AndresArdila89 <andresardila@me.com>
2021-02-23 22:15:05 -0500

    FORM VALIDATION - create a file for validation functions - code a function that allows to find if the string contains numbers - validate first name, last name, city, price and quantity fields - add some comments to the style.css file - remove comments from the head.php file

--- components/head.php
+++ components/head.php
@@ -1,19 +1,3 @@
-<?php
-/**
-   * 2/12/2021: working on the css grid trying to 
-   * make the landing page responsive.
-   * fix the file name of the head from header.php to head.php
-   * 2/16/2021: added a banner with a picture that fits the width
-   * of the page and the container hides the overflow havin a max-height of 600px
-   * fix local issues where the permition of the images folder had no access
-   * 2/16/2021: add the ads container with random images inside an array
-   * one of the images ocupies the 100% of the ads container.
-   * 2/19/2021: added new comments to the code, fix issues with the banner title, 
-   * added navigation between pages, 
-   * 
-   * 
-   */
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
